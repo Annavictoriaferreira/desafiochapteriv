acf3baaaa4fe418b9a9ce1cb6a10f879a1436624
Annavictoriaferreira <anavitoria.f.566@gmail.com>
2022-03-10 10:58:36 -0300

    changes in type

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -14,7 +14,7 @@ describe('Cadastro', () => {
         cy.get('.header_user_info > .login').click();
         cy.url().should('include', '/index.php?controller=authentication&back=my-account')
         //digitar endereço de email no campo: email address
-        cy.get('.form-group > #email_create').should('be.visible').type(chance.email)
+        cy.get('.form-group > #email_create').should('be.visible').type(chance.email())
         //clicar no botão Create an account
         cy.get('.submit > #SubmitCreate').click()
 
@@ -25,8 +25,8 @@ describe('Cadastro', () => {
         //como selecionar outros buttons radios
         cy.get('[type="radio"]').first().check()
 
-        cy.get('.required.form-group > #customer_firstname').should('be.visible').type(chance.first)
-        cy.get('.required.form-group > #customer_lastname').should('be.visible').type(chance.last)
+        cy.get('.required.form-group > #customer_firstname').should('be.visible').type(chance.first())
+        cy.get('.required.form-group > #customer_lastname').should('be.visible').type(chance.last())
         //cy.get('.required.form-group > #email').should('be.visible').type('anna@exxxample.com')
         cy.get('.required.password.form-group > #passwd').should('be.visible').type('aaaaa12389')
 
@@ -38,18 +38,18 @@ describe('Cadastro', () => {
         //Inputs checkbox
         cy.get('#optin').check()
 
-        cy.get('#company.form-control').should('be.visible').type(chance.company)
-        cy.get('#address1.form-control').should('be.visible').type(chance.address)
-        cy.get('#address2.form-control').should('be.visible').type(chance.address)
-        cy.get('#city.form-control').should('be.visible').type(chance.city)
+        cy.get('#company.form-control').should('be.visible').type(chance.company())
+        cy.get('#address1.form-control').should('be.visible').type('rua filie santiago, 222, centro')
+        cy.get('#address2.form-control').should('be.visible').type(chance.address())
+        cy.get('#city.form-control').should('be.visible').type(chance.city())
         
         cy.get('select#id_state').select('Virginia', { force: true })
         
-        cy.get('#postcode.form-control.uniform-input.text').should('be.visible').type(chance.postcode)
-        cy.get('#other.form-control').should('be.visible').type(chance.character)
-        cy.get('#phone.form-control').should('be.visible').type(chance.phone)
-        cy.get('#phone_mobile.form-control').should('be.visible').type(chance.phone)
-        cy.get('#alias.form-control').should('be.visible').type(chance.address)
+        cy.get('#postcode.form-control.uniform-input.text').should('be.visible').type('00000')
+        cy.get('#other.form-control').should('be.visible').type('aaa, 4903928482')
+        cy.get('#phone.form-control').should('be.visible').type(chance.phone())
+        cy.get('#phone_mobile.form-control').should('be.visible').type(chance.phone())
+        cy.get('#alias.form-control').should('be.visible').type(chance.address())
 
         cy.get('.submit.clearfix > #submitAccount').click()
     })
